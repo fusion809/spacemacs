4329cae6083492a2956feab3d6e84df5ba4aa06e
fusion809 <brentonhorne77@gmail.com>
2016-03-24 14:42:46 +1000

    Updating

---
+++ quick-install.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+git clone https://github.com/skeeto/elfeed ~/.emacs.d2/elfeed
+wget -cqO- https://git.io/vaNlS > ~/.emacs.d2/markdown-mode.el
+wget -cqO- https://git.io/vaNlb > ~/.emacs.d2/pkgbuild-mode.el
+wget -cqO- https://git.io/vaN8f > ~/.emacs.d2/neotree.el
+git clone https://github.com/fusion809/spacemacs ~/GitHub/spacemacs
+cp -a ~/GitHub/spacemacs/.spacemacs ~/
